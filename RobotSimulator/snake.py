50be0dd9822c334880300047469c92b107088da9
Eduardo Natale Costa <notebook@pop-os.localdomain>
2019-02-07 17:14:25 -0200

    Modelo de curva

--- RobotSimulator/snake.py
+++ RobotSimulator/snake.py
@@ -1,70 +1,135 @@
-import pygame, random
+import pygame
 from pygame.locals import *
+ 
+class vec2d(object):
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
-def on_grid_random():
-	x = random.randint(0, 590)
-	y = random.randint(0, 590)
-	return (x // 10 * 10, y // 10 * 10)
+ 
+gray = (100,100,100)
+lightgray = (200,200,200)
+red = (255,0,0)
+green = (0,255,0)
+blue = (0,0,255)
+X,Y,Z = 0,1,2
 
-def collision(c1, c2):
-	return (c1[0] == c2[0]) and (c1[1] == c2[1])
+def compute_bezier_points(vertices, numPoints=None):
+    if numPoints is None:
+        numPoints = 30
+    if numPoints < 2 or len(vertices) != 4:
+        return None
 
-UP = 0
-RIGHT = 1
-DOWN = 2
-LEFT = 3
+    result = []
 
-pygame.init()
-screen = pygame.display.set_mode((600, 600))
-pygame.display.set_caption('Snake')
-
-snake = [(200, 200), (210, 200), (220, 200)]
-snake_skin = pygame.Surface((10, 10))
-snake_skin.fill((255, 255, 255))
-
-apple_pos = on_grid_random()
-apple = pygame.Surface((10, 10))
-apple.fill((255, 0, 0))
-
-my_direction = LEFT
-
-clock = pygame.time.Clock()
-
-while True:
-	clock.tick(20)
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-		if event.type == KEYDOWN:
-			if event.key == K_UP:
-				my_direction = UP
-			if event.key == K_DOWN:
-				my_direction = DOWN
-			if event.key == K_LEFT:
-				my_direction = LEFT
-			if event.key == K_RIGHT:
-				my_direction = RIGHT
-
-	if collision(snake[0], apple_pos):
-		apple_pos = on_grid_random()
-		snake.append((0, 0))
-
-	if my_direction == UP:
-		snake[0] = (snake[0][0], snake[0][1] - 10)
-	elif my_direction == DOWN:
-		snake[0] = (snake[0][0], snake[0][1] + 10)
-	elif my_direction == RIGHT:
-		snake[0] = (snake[0][0] + 10, snake[0][1])
-	else:
-		snake[0] = (snake[0][0] - 10, snake[0][1])
-
-	for i in range(len(snake) - 1, 0, -1):
-		snake[i] = (snake[i - 1][0], snake[i - 1][1])
+    b0x = vertices[0][0]
+    b0y = vertices[0][1]
+    b1x = vertices[1][0]
+    b1y = vertices[1][1]
+    b2x = vertices[2][0]
+    b2y = vertices[2][1]
+    b3x = vertices[3][0]
+    b3y = vertices[3][1]
 
 
-	screen.fill((0, 0, 0))
-	screen.blit(apple, apple_pos)
-	for pos in snake:
-		screen.blit(snake_skin, pos)
 
-	pygame.display.update()
+    # Compute polynomial coefficients from Bezier points
+    ax = -b0x + 3 * b1x + -3 * b2x + b3x
+    ay = -b0y + 3 * b1y + -3 * b2y + b3y
+
+    bx = 3 * b0x + -6 * b1x + 3 * b2x
+    by = 3 * b0y + -6 * b1y + 3 * b2y
+
+    cx = -3 * b0x + 3 * b1x
+    cy = -3 * b0y + 3 * b1y
+
+    dx = b0x
+    dy = b0y
+
+    # Set up the number of steps and step size
+    numSteps = numPoints - 1 # arbitrary choice
+    h = 1.0 / numSteps # compute our step size
+
+    # Compute forward differences from Bezier points and "h"
+    pointX = dx
+    pointY = dy
+
+    firstFDX = ax * (h * h * h) + bx * (h * h) + cx * h
+    firstFDY = ay * (h * h * h) + by * (h * h) + cy * h
+
+
+    secondFDX = 6 * ax * (h * h * h) + 2 * bx * (h * h)
+    secondFDY = 6 * ay * (h * h * h) + 2 * by * (h * h)
+
+    thirdFDX = 6 * ax * (h * h * h)
+    thirdFDY = 6 * ay * (h * h * h)
+
+    # Compute points at each step
+    result.append((int(pointX), int(pointY)))
+
+    for i in range(numSteps):
+        pointX += firstFDX
+        pointY += firstFDY
+
+        firstFDX += secondFDX
+        firstFDY += secondFDY
+
+        secondFDX += thirdFDX
+        secondFDY += thirdFDY
+
+        result.append((int(pointX), int(pointY)))
+
+    return result
+    
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((1024, 768))
+ 
+    ### Control points that are later used to calculate the curve
+    control_points = [vec2d(100,100), vec2d(150,500), vec2d(450,500), vec2d(500,150)]
+ 
+    ### The currently selected point
+    selected = None
+    
+    clock = pygame.time.Clock()
+    
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type in (QUIT, KEYDOWN):
+                running = False
+            elif event.type == MOUSEBUTTONDOWN and event.button == 1:
+                for p in control_points:
+                    if abs(p.x - event.pos[X]) < 10 and abs(p.y - event.pos[Y]) < 10 :
+                        selected = p
+            # elif event.type == MOUSEBUTTONDOWN and event.button == 3:
+            #     x,y = pygame.mouse.get_pos()
+            #     control_points.append(vec2d(x,y))
+            elif event.type == MOUSEBUTTONUP and event.button == 1:
+                selected = None
+        
+        ### Draw stuff
+        screen.fill(gray)
+                
+        if selected is not None:
+            selected.x, selected.y = pygame.mouse.get_pos()
+            pygame.draw.circle(screen, green, (selected.x, selected.y), 10)
+        
+        ### Draw control points
+        for p in control_points:
+            pygame.draw.circle(screen, blue, (int(p.x), int(p.y)), 4)
+
+        ### Draw control "lines"
+        pygame.draw.lines(screen, lightgray, False, [(x.x, x.y) for x in control_points])
+
+        ### Draw bezier curve
+        b_points = compute_bezier_points([(x.x, x.y) for x in control_points])
+        pygame.draw.lines(screen, pygame.Color("red"), False, b_points, 2)
+
+        ### Flip screen
+        pygame.display.flip()
+        clock.tick(100)
+        #print clock.get_fps()
+    
+if __name__ == '__main__':
+    main()
